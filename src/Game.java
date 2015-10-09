bc0cbcf14184f77ad72cfccc4478f9fc04e66e58
landonkail <landon.kail@gmail.com>
2015-10-09 11:20:18 -0400

    Add exit command.  Make the game loop

--- src/Game.java
+++ src/Game.java
@@ -7,12 +7,13 @@ public class Game {
     public static void run() throws Exception {
         System.out.println("Welcome to my text adventure.");
 
-        Player player = new Player();
-        player.chooseName();
-        player.chooseWeapon();
-        player.chooseArea();
+        while (true) {
+            Player player = new Player();
+            player.chooseName();
+            player.chooseWeapon();
+            player.chooseArea();
+        }
     }
-
     static String nextLine() {
         Scanner scanner = new Scanner(System.in);
         String s = scanner.nextLine();
@@ -20,6 +21,8 @@ public class Game {
             if (s.equals("/help")) {
                 System.out.println("These are the available commands:");
                 System.out.println("/help => List available commands.");
+            } else if (s.equals("/exit")) {
+                System.exit(0);
             }
             return nextLine();
         } else {
